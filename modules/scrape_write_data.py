a631034a5aacc2a19f791335e454796dfb5b98ee
jrdnbradford <36420801+jrdnbradford@users.noreply.github.com>
2018-10-20 15:49:28 -0400

    Removed unnecessary code and cleaned up comments.

--- modules/scrape_write_data.py
+++ modules/scrape_write_data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 # Scrapes data from Yahoo Finance page and writes to .csv
+# YF puts text w/in <span> tags that are nested in <td> tags (eg <tr><td><span>TEXT</span></td></tr>)
+# If there is no text to display w/in a <td>, then there is no <span> (eg <tr><td>-</td></tr>)
 
 def scrape_write(soup, writer):
     table_tag = soup.table
@@ -13,20 +15,16 @@ def scrape_write(soup, writer):
     for tablerow in tablerow_tags:
         tabledata_tags = tablerow.findAll("td")
         span_tags = tablerow.findAll("span")
+        
         if len(tabledata_tags) > 1: # Don't write descriptive rows w/ only 1 <td>
-            rowcontents = []
-    # YF puts text w/in <span> tags that are nested in <td> tags (eg <tr><td><span>TEXT</span></td></tr>)
-    # If there is no text to display w/in a <td>, then there is no <span> (eg <tr><td>-</td></tr>)
-            if len(tabledata_tags) == len(span_tags): # satisfied if every <td> in row has <span> 
-                for span_tag in span_tags: 
-                    text = span_tag.text
-                    rowcontents.append(text) 
-                writer.writerow(rowcontents)      
-            if len(tabledata_tags) != len(span_tags):
-                for tabledata_tag in tabledata_tags: 
-                    if tabledata_tag.span: # satisfied if <td> in row has <span>
-                        text = tabledata_tag.span.text
-                    else: # satisfied if <td> does not have <span> and placeholder is needed
-                        text = None # No data available
-                    rowcontents.append(text)
-                writer.writerow(rowcontents)            
+            row_contents = []
+            
+            for tabledata_tag in tabledata_tags:
+                # Satisfied if <td> in row has <span> w/ data
+                if tabledata_tag.span:  
+                    text = tabledata_tag.span.text
+                # Satisfied if <td> does not have <span> w/ data and placeholder is needed
+                else: 
+                    text = None
+                row_contents.append(text)
+            writer.writerow(row_contents)            
