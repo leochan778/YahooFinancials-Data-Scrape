bbb14c3a609a424d83449c0c76b590252d737e41
jrdnbradford <36420801+jrdnbradford@users.noreply.github.com>
2018-11-03 10:03:22 -0400

    Switched to requests.

--- modules/nasdaq_symbols.py
+++ modules/nasdaq_symbols.py
@@ -2,20 +2,20 @@
 
 # Gets Nasdaq stock symbols, creates a .txt file, and adds symbols to list
 
-import urllib.request
+import requests
 
 def get_nasdaq():
     # Retrieve, clean, and write Nasdaq data to .txt file
     nasdaq_url = "http://www.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt"
-    nasdaq = urllib.request.urlopen(nasdaq_url)
-    if nasdaq.status == 200:
-        nasdaq_txt = open("nasdaq.txt", "w")
-        for line in nasdaq:
-            # eg AAPL|Apple Inc. - Common Stock|Q|N|N|100|N|N
-            line = line.decode("utf-8")
-            symbol = line.split("|")[0]
-            nasdaq_txt.write(symbol + "\n") 
-        nasdaq_txt.close()
+    nasdaq = requests.get(nasdaq_url, timeout = 2)
+    
+    if nasdaq.status_code == 200:
+        with open("nasdaq.txt", "w") as nasdaq_txt:
+            for line in nasdaq.iter_lines():
+                # eg AAPL|Apple Inc. - Common Stock|Q|N|N|100|N|N
+                line = line.decode("utf-8")
+                symbol = line.split("|")[0]
+                nasdaq_txt.write(symbol + "\n") 
 
     # Take stock symbols from created .txt file and add them to list
     stock_symbols = [line.strip() for line in open("nasdaq.txt", "r")]
