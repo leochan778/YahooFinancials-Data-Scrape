10a7c37dd90f2037fd7b60f0c4de701c7bd9721c
jrdnbradford <36420801+jrdnbradford@users.noreply.github.com>
2019-04-05 21:39:11 -0400

    Simplify and split nasdaq functionality.

--- modules/nasdaq_symbols.py
+++ modules/nasdaq_symbols.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
-
-# Gets Nasdaq stock symbols, creates a .txt file, and adds symbols to list
-
 import requests
 
-def get_nasdaq():
-    # Retrieve, clean, and write Nasdaq data to .txt file
+
+def request_nasdaq():
+    """
+    Sends HTTP request for Nasdaq data and writes it to .txt file,
+    then calls get_nasdaq() to get stock symbols.
+    """
     nasdaq_url = "http://www.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt"
-    nasdaq = requests.get(nasdaq_url, timeout = 2)
-    
+    nasdaq = requests.get(nasdaq_url, timeout=2)
     if nasdaq.status_code == 200:
         with open("nasdaq.txt", "w") as nasdaq_txt:
-            for line in nasdaq.iter_lines():
-                # eg AAPL|Apple Inc. - Common Stock|Q|N|N|100|N|N
-                line = line.decode("utf-8")
-                symbol = line.split("|")[0]
-                nasdaq_txt.write(symbol + "\n") 
+            nasdaq_txt.write(nasdaq.content.decode("utf-8"))
+        return get_nasdaq()
+    else:
+        raise Exception(f"HTTP status code is {nasdaq.status_code}")
 
-    # Take stock symbols from created .txt file and add them to list
-    stock_symbols = [line.strip() for line in open("nasdaq.txt", "r")]
 
-    # nasdaq.txt and stock_symbols may include non-symbol data which will be caught by error handling
-    return stock_symbols
+def get_nasdaq():
+    """Gets stock symbols from local nasdaq.txt file."""
+    return [line.strip().split("|")[0] for line in open("nasdaq.txt", "r")][1:]
